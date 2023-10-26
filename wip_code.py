57da95ded4dfbecc73969794146581df0b73a8c7
Leo Paggen <lpaggen@gmail.com>
2023-10-26 18:56:18 +0200

    bug fixes

--- wip_code.py
+++ wip_code.py
@@ -7,10 +7,19 @@ class SimplexTools():
     obj_fnc = [0.0] # starts with a 0 to account for the constant in the constraints while tranforming problem to dictionary format
     constraints = [] # empty list of constraints, used to create a system of equations later on
     dictionaries = [] # list of dictionaries, essentially what every other method operates on and modifies
-    exit_new_coords = []
+    pivot_iteration = 0 # keeps track of how the pivot iteration
+    iter_tracker = {}
+    prev_enter_coordinates = {}
+
     def __init__(self, n, m): # will potentially add an option to have the problem as a minimization or a maximization problem
         self.var_count = n # number of variables to the problem
         self.constr_count = m # number of bounds to the problem
+
+        # dict to keep track of iterations PER constraint
+        for i in range(0, self.constr_count): # starts at 0, indexing similar to normal list
+            constr = f"{i}" # makes future statements easier, rather than using some complex naming
+            self.iter_tracker[constr] = 1 # sets the value of each constraint iteration count to 1
+            self.prev_enter_coordinates[constr] = 0
 
     # implementing a way to store the information of a LP problem (vaiables, constraints)
     def _formulate_problem(self): # n variables, m constraints
@@ -70,19 +79,11 @@ class SimplexTools():
     def _pivot(self):
         """Identifies the column and row to pivot for which a variable will enter the basis"""
         # setting up the basic parameters of the function
-        pivot_iteration = 0 # general pivot iteration, only used for descriptive statistics at the end
         dictionary = self.dictionaries[0] # fetches the problem as transformed by "_formulate_problem()"
         print(dictionary) # debug, works as intented
-        
-        # dictionary which keeps track of the current iteration of each constraint (used mainly for 1st iteration)
-        iteration_tracker = {}
-        for i in range(0, self.constr_count): # starts at 0, indexing similar to normal lists etc
-            constr = f"{i}" # makes future statements easier, rather than using some complex naming
-            iteration_tracker[constr] = 1 # sets the value of each constraint iteration count to 1
-            print(iteration_tracker)
 
         # find the variable with highest coeff in obj function
-        pivot_col_index = np.argmax(dictionary[0, 1:]) + 1 # Z column is excluded, add 1
+        pivot_col_index = np.argmax(dictionary[0, 2:]) + 2 # Z AND optimal value excl.
         pivot_col_no_obj = dictionary[1:, pivot_col_index].astype(np.float64)
         pivot_col = dictionary[:, pivot_col_index].astype(np.float64)
 
@@ -99,56 +100,98 @@ class SimplexTools():
 
         # finding the coordinates ij of entering and exiting variables before the pivot
         entering_var_coord = pivot_row_index, pivot_col_index # this is a tuple of 2 coordinates
-        print(entering_var_coord) # debug
+        print(f"entering_var: {entering_var_coord}") # debug
         exiting_var_coord = pivot_row_index, 0 # tuple of 2 coordinates
-        print(exiting_var_coord) # debug
+        print(f"exiting_var: {exiting_var_coord}") # debug
         iteration_tracker_index = str(exiting_var_coord[0] - 1)
         print(iteration_tracker_index) # debug
 
-        # appends coordinates ij of entering variable to a list
-        self.exit_new_coords.append(entering_var_coord)
+        # dictionary of entering_var coordinates
+        # prev_enter_coordinates = {}
+        # for i in range(0, self.constr_count): # starts at 0, indexing similar to normal lists etc
+        #     constr = f"{i}" # makes future statements easier, rather than using some complex naming
+        #     prev_enter_coordinates[constr] = 0
+
+        # coordinates of entering var before pivot (tuple) get stored in dictionary
+        constr_index = str(entering_var_coord[0] - 1)
+        self.prev_enter_coordinates[constr_index] = entering_var_coord
 
         # assign the coordinates ij of entering and exiting variables after the pivot
-        if iteration_tracker.get(iteration_tracker_index) == 1: # checks if the iteration of the pivot row is 1
-            exiting_var_coord_after_pivot = pivot_row_index, (1 + self.var_count + pivot_row_index)
-            print(exiting_var_coord_after_pivot) # debug
+        if self.iter_tracker.get(iteration_tracker_index) == 1: # checks if the iteration of the pivot row is 1
+            exiting_var_coord_after_pivot = pivot_row_index, (int(iteration_tracker_index) + self.var_count + 2) #temporary
+            print(f"exiting var goes to -> {exiting_var_coord_after_pivot}") # debug
+            self.iter_tracker[iteration_tracker_index] += 1 # increment pivot iter by 1
         else:
-            exiting_var_coord_after_pivot = self.exit_new_coords[0] # entering var goes back to correct place
+            exiting_var_coord_after_pivot = self.prev_enter_coordinates[constr_index] # entering var goes back to correct place
+            print(f"all entering var positions -> {self.prev_enter_coordinates}")
+            print(f"exiting var goes to -> {exiting_var_coord_after_pivot}")
 
         # exiting variable pivots out of the basis
         dictionary[exiting_var_coord_after_pivot] = -exiting_var # change coefficient sign
         dictionary[exiting_var_coord] = 0.0 # 0 assigned to where exiting var was pre pivot
-        print(dictionary) # debug
 
         # multiplication ratio by which to add pivot row to other rows (incl obj row)
-        pivot_col_no_entering_var = [i for i in pivot_col if i != entering_var] # excl entering_var
-        mult_ratio = pivot_col_no_entering_var / entering_var
-        print(mult_ratio) # works here still
-        print("this is fine")
+        pivot_col_no_enter_var_coords = []
+        for i in range(self.constr_count + 1):
+            if i != pivot_row_index:
+                coords = i, pivot_col_index
+                pivot_col_no_enter_var_coords.append(coords)
+        print(f"pivot row no entervar -> {pivot_col_no_enter_var_coords}")
+        pivot_col_no_enter_var = [dictionary[i] for i in pivot_col_no_enter_var_coords]
+        mult_ratio = pivot_col_no_enter_var / entering_var
+        print(f"Multiplication Ratio -> {mult_ratio}")
 
         # get rid of instances of entering variable in other rows
-        print(pivot_row) # debug
         tolerance = 1e-10 # small tolerance for comparing floats
         mult_ratio_index = 0
         for row in range(0, self.constr_count + 1):
-            print("entering loop")
-            print(dictionary[row, :])
             row_to_modify = np.all(np.isclose(dictionary[row, :], pivot_row, atol = tolerance))
-            print(row_to_modify)
             if not row_to_modify:
-                print("working until now")
                 dictionary[row, :] = dictionary[row, :] - (mult_ratio[mult_ratio_index] * pivot_row)
                 mult_ratio_index = (mult_ratio_index + 1) % len(mult_ratio)
             else:
                 pass
-        print(dictionary)
-        print("this works")
+        print(f"mult ratio index -> {mult_ratio_index}")
 
         # entering variable pivots into the basis
         dictionary[exiting_var_coord] = -entering_var # change signs
         dictionary[entering_var_coord] = 0.0
+        dictionary[pivot_row_index, :] = dictionary[pivot_row_index, :] / dictionary[exiting_var_coord]
+        print("\nAfter pivoting:\n")
         print(dictionary)
 
-    def solve(self):
-        """Solves an LP maximization problem and gives optimal solutions to the problem"""
-        # set entering variable for the _pivot method
+        # update the dictionary in the class list
+        self.dictionaries[0] = dictionary
+
+        # increment pivot by 1
+        self.pivot_iteration += 1
+
+def solve(n, m):
+    """
+    Returns the solutions to an LP maximization problem
+    
+    Parameters: n = number of variables in the problem
+                m = number of constraints in the problem
+    """
+    problem = SimplexTools(n, m)
+    problem._formulate_problem()
+    problem._to_dict()
+    tableau = problem.dictionaries[0]
+    print("solve tableau")
+    print(tableau)
+    obj_fnc = tableau[0, 2:]
+    print(obj_fnc)
+    print("yes yes yes yes yes")
+    while np.any(obj_fnc > 0):
+        problem._pivot()
+    print(tableau)
+    print("problem solved")
+
+def main(n,m):
+    simplex = SimplexTools(n,m)
+    simplex._formulate_problem()
+    simplex._to_dict()
+    simplex._pivot()
+
+if __name__ == "__main__":
+    main(2,3)
